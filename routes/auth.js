363400dc0132ddc7e79f2c4e958d12960a3eac50
MD SALIM <mdsalim.iiitr@gmail.com>
2023-04-07 11:04:37 +0530

    Created thunderclient Collections to Magnage Requests, added functionality to check wheter a user with the given email already exists or not, if exists then give erro(400) and not create the new user

--- routes/auth.js
+++ routes/auth.js
@@ -1,27 +1,47 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const User = require('../models/User');
+const User = require("../models/User");
 
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
-// create a User using: POST "api/auth/". Doesn't require auth
-router.post('/',[
-    body('name', 'Enter a valid name').isLength({ min: 3 }),
-    body('email', 'Enter a valid email').isEmail(),
-    body('password', 'Password must be atleast 5 characters').isLength({ min: 5 }),
-] , (req, res)=>{
+// create a User using: POST "api/auth/createuser". No login required
+router.post(
+  "/createuser",
+  [
+    body("name", "Enter a valid name").isLength({ min: 3 }),
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    // If there are errors, return Bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
+    try {
+      // Check whether the user with this email exists already
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user this email already exists" });
+      }
+      // Creating a new user accordin to User schema (date is by default set)
+      user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
-      }).then(user => res.json(user))
-      .catch(err => {console.log(err)
-    res.json({error: 'Please enter a unique value for email', message: err.message})});
-})
+        password: req.body.password,
+      });
+      res.json(user);
+      // Catch errors
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("some error occured");
+    }
+  }
+);
 
 module.exports = router;
