b407b4af75a6c952cad1663e3a827233684aa0c1
MD SALIM <mdsalim.iiitr@gmail.com>
2023-04-08 13:42:27 +0530

    Adding notes and fetching all notes using auth-token(JWT)

--- models/Notes.js
+++ models/Notes.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
     title:{
         type: String,
         required: true
     },
     description:{
-        type: string,
+        type: String,
         required: true,
     },
     tag:{
-        type: string,
+        type: String,
         default: "General"
     },
     date:{
